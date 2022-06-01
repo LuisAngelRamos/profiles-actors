80c62786ee8091cdaec52c31215c48e03a48bf2a
Mofles11 <luisangelramosmejia11@gmail.com>
2022-06-01 08:07:31 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./assets/styles/App.scss";
 import { Articles } from "./components/Articles/Articles";
 import { Profile } from "./components/Profile/Profile";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  
   return (
     <>
       <Profile />
